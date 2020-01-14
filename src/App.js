6a357028692e97b2c5e3fccf3867e6f92893af34
user <user@localhost.localdomain>
2020-01-13 20:04:45 -0500

    Added new feature text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello !!!
+          This is new feature
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn react
         </a>
       </header>
     </div>
